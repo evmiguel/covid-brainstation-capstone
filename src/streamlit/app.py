fc671c7d811f8a3b22baaf15c06a046604cb075f
Erika Miguel <erika@erikamiguel.com>
2024-09-24 21:44:57 -0400

    Sprint 3: updating app

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -7,17 +7,33 @@ import torch
 import torch.nn as nn
 import sys; sys.path.insert(0, '.')
 from lib import df_processing
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from sklearn.metrics import mean_absolute_error, r2_score
 import datetime
 
 #######################################################################################
 # CONFIGURATION
 #######################################################################################
 
-st.set_page_config(page_title="COVID Staff Shortage Predictor")
+st.set_page_config(page_title="COVID Staff Shortage Predictor", layout="wide")
 
 def get_feature_name(feature):
     return feature.split("-")[0] if len(feature.split("-")) == 2 else feature.split("-")[0] + "-" + feature.split("-")[1]
+
+def highlight_max(s, df):
+    name = s.name
+    cells = []
+    if name == "Critical Staffing Shortage Today Yes":
+        second = np.argsort(df.T)[1]
+        for i, cell in enumerate(s):
+            if i == second:
+                cells.append("background-color: green")
+            elif i == 0:
+                cells.append("background-color: orange")
+            else:
+                cells.append(None)
+        return cells
+    return [None, None, None, None]
 
 #######################################################################################
 # NEURAL NET FOR LOADING MODEL
@@ -88,11 +104,6 @@ with open("./data/r2_pkl_xgb.json", 'r') as file:
 with open("./data/r2_pkl_nn.json", 'r') as file:
     r2_pkl_nn = json.load(file)
 
-best_model = st.empty()
-r2 = st.empty()
-prediction_lr = st.empty()
-prediction_xgb = st.empty()
-prediction_nn = st.empty()
 lag = 1
 
 #######################################################################################
@@ -103,13 +114,13 @@ region = st.sidebar.selectbox("Select a region", ["New England", "Mid Atlantic",
 
 
 if region == "New England":
-    options = [datetime.datetime(2021, 1, 1), datetime.datetime(2021, 7, 1), datetime.datetime(2024, 4, 1)]
-elif region == "Mid Atlantic":
-    options = [datetime.datetime(2020, 11, 1), datetime.datetime(2020, 12, 1), datetime.datetime(2021, 12, 1), datetime.datetime(2022, 5, 1), datetime.datetime(2022, 8, 1), datetime.datetime(2024, 4, 1)]
-elif region == "Midwest":
-    options = [datetime.datetime(2020, 8, 1), datetime.datetime(2021, 7, 1), datetime.datetime(2022, 2, 1), datetime.datetime(2024, 4, 1)]
-elif region == "South":
     options = [datetime.datetime(2021, 1, 1), datetime.datetime(2021, 7, 1), datetime.datetime(2022, 1, 1), datetime.datetime(2024, 4, 1)]
+elif region == "Mid Atlantic":
+    options = [datetime.datetime(2020, 12, 15), datetime.datetime(2021, 3, 1), datetime.datetime(2021, 12, 1), datetime.datetime(2022, 5, 1), datetime.datetime(2022, 8, 1), datetime.datetime(2024, 4, 1)]
+elif region == "Midwest":
+    options = [datetime.datetime(2020, 12, 1), datetime.datetime(2021, 7, 1), datetime.datetime(2022, 1, 1), datetime.datetime(2024, 4, 1)]
+elif region == "South":
+    options = [datetime.datetime(2021, 1, 1), datetime.datetime(2021, 7, 1), datetime.datetime(2021, 9, 1), datetime.datetime(2024, 4, 1)]
 elif region == "Southwest":
     options = [datetime.datetime(2020, 8, 1), datetime.datetime(2021, 1, 1), datetime.datetime(2021, 7, 1), datetime.datetime(2022, 1, 1), datetime.datetime(2024, 4, 1)]
 else:
@@ -118,9 +129,12 @@ else:
 chosen_date = st.sidebar.select_slider("Select a date", options=options)
 
 min_default_date = dfs_region_map[region].index.min().to_pydatetime()
+max_default_date = dfs_region_map[region].index.max().to_pydatetime()
 
 lag = st.sidebar.select_slider("Lag", options=[1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+lag_text = st.header(f"Lag: {lag}")
 rolling_df = df_processing.create_rolling_df(dfs_region_map[region], lag, ignore_columns=["critical_staffing_shortage_today_yes"])
+actual_shortage = dfs_region_map[region].loc[dfs_region_map[region].index == chosen_date, "critical_staffing_shortage_today_yes"]
 
 ##################################################################
 # LINEAR REGRESSION
@@ -155,24 +169,34 @@ input_df = rolling_df.loc[rolling_df.index == chosen_date, columns]
 scaled_input = preprocessor.transform(input_df)
 shortage_nn = model(torch.tensor(scaled_input, dtype=torch.float32)).item()
 
+##################################################################
+# Dataframes
+##################################################################
+shortages = [round(actual_shortage.values[0], 2), round(shortage_lr, 2), round(shortage_xgb, 2), round(shortage_nn, 2)]
+shortages_df = pd.DataFrame(data=shortages, index=["Actual", "Linear Regression", "XGBoost", "Neural Net"], columns=["Critical Staffing Shortage Today Yes"])
+errors = [mean_absolute_error(actual_shortage, actual_shortage), mean_absolute_error(actual_shortage, [shortage_lr]), mean_absolute_error(actual_shortage, [shortage_xgb]), mean_absolute_error(actual_shortage, [shortage_nn])]
+errors_df = pd.DataFrame(data=errors, index=["Actual", "Linear Regression", "XGBoost", "Neural Net"], columns=["Mean Absolute Error"])
+top_df = pd.concat([shortages_df, errors_df], axis=1)
+st.dataframe(top_df.T.style.apply(highlight_max, axis=1, df=top_df.T.loc["Mean Absolute Error"]))
 
 ##################################################################
 # Plotting
 ##################################################################
-fig, ax = plt.subplots()
-ax.set_xlim(min_default_date - datetime.timedelta(days=60), chosen_date + datetime.timedelta(days=60))
-ax.plot(dfs_region_map[region].loc[dfs_region_map[region].index <= chosen_date, "critical_staffing_shortage_today_yes"], alpha=0.2)
-ax.plot(chosen_date, dfs_region_map[region].loc[dfs_region_map[region].index == chosen_date, "critical_staffing_shortage_today_yes"], "go")
-ax.plot(chosen_date, shortage_lr, color="red", marker="o")
-ax.plot(chosen_date, shortage_xgb, color="blue", marker="*")
-ax.plot(chosen_date, shortage_xgb, color="orange", marker="X")
-for tick in ax.get_xticklabels():
-    tick.set_rotation(90)
-st.pyplot(fig)
-
-prediction_lr.text(f"Staffing shortage Linear Regression: {round(shortage_lr)}")
-prediction_xgb.text(f"Staffing shortage XGBoost: {round(shortage_xgb)}")
-prediction_nn.text(f"Staffing shortage Neural Net: {round(shortage_nn)}")
+fig = go.Figure()
+line_graph = go.Line(x=pd.date_range(min_default_date, max_default_date), y=dfs_region_map[region].loc[:, "critical_staffing_shortage_today_yes"], name="critical staffing shortage over time", opacity=0.5)
+actual_point = go.Scatter(x=[chosen_date], y=actual_shortage, marker_symbol="x", marker_size=15, marker_color="#1BFC06", name="actual shortage value")
+lr_point = go.Scatter(x=[chosen_date], y=[shortage_lr], marker_symbol="circle", marker_color="red", marker_size=10, name="Linear regression prediction")
+xgb_point = go.Scatter(x=[chosen_date], y=[shortage_xgb], marker_symbol="star", marker_color="blue", marker_size=10, name="XGB prediction")
+nn_point = go.Scatter(x=[chosen_date], y=[shortage_nn], marker_symbol="triangle-up", marker_color="orange", marker_size=10, name="Neural net prediction")
+fig.add_trace(line_graph)
+fig.add_trace(actual_point)
+fig.add_trace(lr_point)
+fig.add_trace(xgb_point)
+fig.add_trace(nn_point)
+fig.update_layout(height=600, legend={
+            "x": 0.8,
+            "y": 0.9})
+st.plotly_chart(fig, use_container_width=True)
 
 
 
